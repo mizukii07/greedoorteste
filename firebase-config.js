18317f6e70388f06e7683a0985d824ac79269637
mizukii07 <marcos.fteixeira.07@gmail.com>
2025-08-18 16:20:31 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyArKOkY72tx9qOelx_oeMnUuS4XZe58df4",
+  apiKey: "AIzaSyArKOkY72tx9qOelx_oeMnUu极狐S4XZe58df4",
   authDomain: "green-door-rpg.firebaseapp.com",
   projectId: "green-door-rpg",
   storageBucket: "green-door-rpg.appspot.com",
@@ -8,7 +7,6 @@ const firebaseConfig = {
   appId: "1:1062933456039:web:c292177334060d46a1e782"
 };
 
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
