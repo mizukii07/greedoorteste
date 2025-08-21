393247bf5ef59d7615a529ec6085289567310f91
mizukii07 <marcos.fteixeira.07@gmail.com>
2025-08-21 16:49:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyArKOkY72tx9qOelx_oeMnUuS4XZe58df4",
-  authDomain: "green-door-rpg.firebaseapp.com",
-  projectId: "green-door-rpg",
-  storageBucket: "green-door-rpg.appspot.com",
-  messagingSenderId: "1062933456039",
-  appId: "1:1062933456039:web:c292177334060d46a1e782"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-
 const bonusAltura = {
   '1.50-1.55': { drible: 3, explosao: 2, reflexos: 2, cabeceio: -3, intimidacao: -2, defesa: -2 },
   '1.56-1.60': { drible: 2, explosao: 2, reflexos: 2, cabeceio: -3, intimidacao: -2, defesa: -1 },
